87cc0dc675d066a21bd6f9584635860365185317
Mikhail Russell <mikhailrussell@outlook.com>
2023-05-09 10:19:03 +0200

    Worked more on pancham, managed to get it to read text file

--- Pancham_implementation/Pancham_implementation.srcs/sim_1/new/testbench.v
+++ Pancham_implementation/Pancham_implementation.srcs/sim_1/new/testbench.v
@@ -32,27 +32,50 @@ module testbench();
     reg reset;
     wire rdy;
     pancham p( .clk(clk), .msg_in(msg), .msg_in_width(width),.msg_in_valid(val),.reset(reset),.ready(rdy));
-    fetcher f(.clk(clk));
-    
+    //fetcher fetch(.clk(clk));
+    integer a1;
+    integer i;
+    integer r;
+    reg [7:0] data[0:3];  // Array of four 8-bit characters
     initial
-    begin 
-    clk = 1'b0;
-    msg = "olleh";//40'b0110111101101100011011000110010101101000;//b0110100001100101011011000110110001101111;//b0110111101101100011011000110010101101000;//b0110100001100101011011000110110001101111
-   // msg = 40'b0;
-    width = 40;
-    val = 0;
-    reset = 1;
-    #1
-    reset = 0;
-    val = 1;
+        begin 
+        clk = 1'b0;
+        msg = "olleh";//40'b0110111101101100011011000110010101101000;//b0110100001100101011011000110110001101111;//b0110111101101100011011000110010101101000;//b0110100001100101011011000110110001101111
+       // msg = 40'b0;
+        width = 40;
+        val = 0;
+        reset = 1;
+        #1
+        reset = 0;
+        val = 1;
+        
+        a1= $fopen("passwordList.txt","r");
+            // Read the first two integers from the file
+        for (i = 0; i < 4; i = i + 1) begin
+          $fscanf(a1, "%c", data[i]);
+        end
+    
+        // Close the file
+        //$fclose(a1);
+        $display("data %h", data[0]);
+
     end
     always 
     begin
-        #clockRate clk = ~clk; //Clock Rate of 0.2 gives #1 is one nano second delay (#x controlls the speed)
-//        if (rdy == 1) begin
-//            msg = msg + 1;
-//        end
-            
-         
+        #clockRate clk = ~clk;
+  
+    end
+    
+    always @ (posedge clk) begin
+        
+        if(rdy == 1) begin
+            for(i = 0; i < 1; i = i + 1) begin
+                $fscanf(a1, "%c", r);
+            end
+            for (i = 0; i < 4; i = i + 1) begin
+               $fscanf(a1, "%c", data[i]);
+            end
+        end
+    
     end
 endmodule
--- Pancham_implementation/Pancham_implementation.srcs/sources_1/new/fetcher.v
+++ Pancham_implementation/Pancham_implementation.srcs/sources_1/new/fetcher.v
@@ -1,42 +1,16 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 07.05.2023 13:04:57
-// Design Name: 
-// Module Name: fetcher
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
+
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module fetcher( input clk
-
-    );
-  integer file_handle;
-  integer data1, data2;
+module fetcher;
+  
+  
+  integer a1;
+  
 
   initial begin
-    // Open the file
-    file_handle = $fopen("passwordList.txt", "r");
-
-    // Read the first two integers from the file
-    $fscanf(file_handle, "%d %d", data1, data2);
-
-    // Close the file
-    $fclose(file_handle);
-
-    // Print the data to the console
-    $display("Data read from file: %d, %d", data1, data2);
+    #10;
+     a1= $fopen("/passwordList.txt","r");
   end
 endmodule
