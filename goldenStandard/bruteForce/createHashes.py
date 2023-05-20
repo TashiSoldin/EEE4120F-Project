0d139dd09b73a034d0710ee97f3d63eff968b6ef
roryschram <66696753+roryschram@users.noreply.github.com>
2023-05-21 00:45:55 +0200

    Update createHashes.py

--- goldenStandard/bruteForce/createHashes.py
+++ goldenStandard/bruteForce/createHashes.py
@@ -1,5 +1,6 @@
 from hashlib import md5
 
+# Hash all the passwords from the brute force password list. This function is used for testing purposes only
 with open("passwordList.txt", "r") as passwords:
     with open("hashedPaswords.txt", "w") as hashes:
         for line in passwords.readlines():
